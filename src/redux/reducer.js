4e1f0a1987ec68b5ce1dabcb8736bfd3bad00d18
arjohn.ely45@gmail.com <arjohn.ely@fligno.com>
2021-09-21 15:48:56 +0800

    fixex cart calculation in redux

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -10,15 +10,11 @@ const cartItems = (state = initialState, action) => {
         case ADD_TO_CART:
             // checking item existence in the cart
             const inCart = state.items.some(item => item.id === action.payload.id);
-            // grand total calculation
-            const cartTotalAmount = state.items.reduce(
-                (totalAmount, { itemPrice, itemQty }) => totalAmount + itemPrice * itemQty,
-                0,
-              );
-              
+            // checking if the item exist and selected
+            const itemChecked = state.items.some(item => item.itemChecked === 1 && item.id === action.payload.id);
             if (inCart) {
                 // already in cart, shallow copy cart items
-                if(action.payload.itemChecked === 1){
+                if(itemChecked){
                   return {
                     ...state,
                     items: state.items.map(item => item.id === action.payload.id ? {
@@ -36,7 +32,6 @@ const cartItems = (state = initialState, action) => {
                       ...item,
                       itemQty: item.itemQty + 1,
                     } : item),
-                    total: cartTotalAmount + action.payload.itemPrice 
                   }
                 }
 
@@ -48,11 +43,18 @@ const cartItems = (state = initialState, action) => {
               }
 
         case REMOVE_FROM_CART:
+          if(action.payload.itemChecked === 1){
             return {
-                ...state,
-                total: state.total - action.payload.itemPrice * action.payload.itemQty,
-                items: state.items.filter(item => item.id !== action.payload.id),
+              ...state,
+              total: state.total - action.payload.itemPrice * action.payload.itemQty,
+              items: state.items.filter(item => item.id !== action.payload.id),
             }
+          }else{
+            return {
+              ...state,
+              items: state.items.filter(item => item.id !== action.payload.id),
+            }
+          }
 
         case ADD_ITEM_QTY:
           if(action.payload.itemChecked === 1){
@@ -105,7 +107,7 @@ const cartItems = (state = initialState, action) => {
                   items: state.items.map(item => item.id === action.payload.id ? {
                     // found item, shallow copy item and update quantity property
                     ...item,
-                    itemChecked: item.itemChecked === 1 ? 0:1
+                    itemChecked: item.itemChecked === 1 ? 0 : 1
                   } : item),
                   total: state.total - action.payload.itemPrice * action.payload.itemQty
                 }
@@ -116,7 +118,7 @@ const cartItems = (state = initialState, action) => {
                   items: state.items.map(item => item.id === action.payload.id ? {
                     // found item, shallow copy item and update quantity property
                     ...item,
-                    itemChecked: item.itemChecked === 0 ? 1:0
+                    itemChecked: item.itemChecked === 0 ? 1 : 0
                   } : item),
                   total: state.total + action.payload.itemPrice * action.payload.itemQty
                 } 
